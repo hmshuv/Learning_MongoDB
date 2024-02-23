e6fbe391e571eddc20d218a25dec131a7abe127f
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-02-24 01:24:31 +0530

    i

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
-const express = require("express");
+const express = required("express");
 const app = express();
-const mongoose = require("mongoose");
+const mongoose = required("mongoose");
 
 
 //schema
 
-const students = new mongoose.Schema({
+const studentSchema = new mongoose.Schema({
     name: {
         type: string,
         unique: false,
-        requre: true,
+        required: true,
     },
     middlename: {
         type:string,
-        require: false,
+        required: false,
         unique: false,
     },
     lastname: {
         type: string,
-        require: true,
+        required: true,
         unique: false,
     },
     rollno: {
         type: integer,
-        require: true,
+        required: true,
         unique: true,
     },
     email: {
         type: string,
-    }
-})
+        required: true,
+        unique: true,
+    },
+    DOB: {
+        type: Character,
+        required: true,
+        unique: true,
+    },
+    address: {
+        type: string,
+        required: true,
+        unique: false,
+    },
+});
+
+
+// model
+const Student = mongoose.model("students", studentSchema);
+
+//connecting 
+mongoose.connect("mongodb://127.0.0.1:27017/students-details")
+.then(()=> console.log("MongoDB connected!"))
+.catch((err)=> console.log("Mongo Error", err));
