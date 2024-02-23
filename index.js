8678f0a756dd1a80d39161a55b0cd5e2941ec81a
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-02-24 02:13:43 +0530

    i

--- index.js
+++ index.js
@@ -51,3 +51,37 @@ const Student = mongoose.model("students", studentSchema);
 mongoose.connect("mongodb://127.0.0.1:27017/students-details")
 .then(()=> console.log("MongoDB connected!"))
 .catch((err)=> console.log("Mongo Error", err));
+
+//routing
+
+
+
+app.post("/student-details", async(req, res)=>{
+    const body = req.body;
+    if(
+     !body||
+     !body.first_name||
+     !body.middle_name||
+     !body.last_name||
+     !body.roll||
+     !body.email||
+     !body.DOB||
+     !body.address
+
+    ){
+        res.status(400).json({msg:"All fields are req..."})
+    }
+    const result = await Student.create({
+        firstname: body.first_name,
+        middlename: body.middle_name,
+        lastname: body.last_name,
+        rollno: body.roll,
+        email:body.email,
+        DOB: body.DOB,
+        address: body.address,
+    });
+    console.log("result", result);
+    return res.status(201).json({msg:"success"});
+});
+
+app.listen(5600, ()=> console.log("server listening to 5600 for mongodb"))
